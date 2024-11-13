60a925fd0668d1de434adae418d72727464420cc
arifmalabar <ridhoarif40@gmail.com>
2024-11-13 18:34:27 +0700

    add table bom & detail bom

---
+++ database/migrations/2024_11_13_112641_create_table_billofmaterialsdetails.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableBillofmaterialsdetails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_billofmaterialsdetails', function (Blueprint $table) {
+            $table->char("id", 20);
+            $table->bigInteger("components_id")->unsigned();
+            $table->float("quantity");
+            $table->integer("price")->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_billofmaterialsdetails');
+    }
+}
