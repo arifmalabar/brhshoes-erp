a0a5087d2707b72f2a889f7ef0ce072895bbdaca
arifmalabar <ridhoarif40@gmail.com>
2024-11-18 22:39:48 +0700

    seeder bom

--- app/Models/ManufaturingOrder.php
+++ app/Models/ManufaturingOrder.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class ManufaturingOrder extends Model
 {
     public $timestamps = false;
     use HasFactory;
